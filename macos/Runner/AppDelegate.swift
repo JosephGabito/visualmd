eb5e114ed845e8be29fa06180a3e00dae1d01460
Joseph Gabito <81974552+JosephGabito@users.noreply.github.com>
2026-08-28 18:34:30 +0800

    fix(macos): preserve app across window closure

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -4,6 +4,16 @@ import FlutterMacOS
 @main
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    false
+  }
+
+  override func applicationShouldHandleReopen(
+    _ sender: NSApplication,
+    hasVisibleWindows flag: Bool
+  ) -> Bool {
+    if !flag {
+      mainFlutterWindow?.makeKeyAndOrderFront(self)
+    }
     return true
   }
 
